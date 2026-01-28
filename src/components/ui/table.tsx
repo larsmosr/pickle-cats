ae546c6dc314cf430cfda622551e3f4903d2463b
Lars Moser <moser.lars@mail.de>
2026-01-28 23:24:02 +0700

    feat. add all

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -31,7 +31,7 @@ function TableRow({ className, ...props }: React.ComponentProps<'tr'>) {
   return (
     <tr
       data-slot="table-row"
-      className={cn('hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors', className)}
+      className={cn('data-[state=selected]:bg-muted border-b', className)}
       {...props}
     />
   )
@@ -54,7 +54,7 @@ function TableCell({ className, ...props }: React.ComponentProps<'td'>) {
   return (
     <td
       data-slot="table-cell"
-      className={cn('p-3 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0', className)}
+      className={cn('p-3 align-middle whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0', className)}
       {...props}
     />
   )
